ba6be24681a1be48143eb476e36cde75450c62c4
alexMACTEP <a069ae@yandex.ru>
2023-09-14 09:38:15 +0300

    calculator

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,54 @@
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Integer[] numbers = {1, -2, 32, 0, 6, 8, 1};
-
-        for (int i : sort(numbers)) {
-            System.out.println(i);
-        }
-    }
-
-    public static Integer[] sort(Integer[] mas) {
-
-        boolean flag = true;
-        int temp;
-
-        while (flag) {
-            flag = false;
-            for (int i = 0; i < mas.length - 1; i++) {
-                if (mas[i] > mas[i + 1]) {
-                    temp = mas[i];
-                    mas[i] = mas[i + 1];
-                    mas[i + 1] = temp;
-                    flag = true;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите  число: ");
+            double num1 = scanner.nextDouble();
+            boolean f = true;
+            while (f) {
+                System.out.print("Введите оператор (В случае выходы введите - 'e'): ");
+                char operator = scanner.next().charAt(0);
+                if (operator == 'e') {
+                    System.out.println("вы завершили работу");
+                    f = false;
+                    break;
                 }
+
+                System.out.print("Введите число: ");
+                double num2 = scanner.nextDouble();
+
+                switch (operator) {
+                    case '+':
+                        num1 = num1 + num2;
+                        break;
+                    case '-':
+                        num1 = num1 - num2;
+                        break;
+                    case '*':
+                        num1 = num1 * num2;
+                        break;
+                    case '/':
+                        if (num2 == 0) {
+                            System.err.println("Делить на ноль нельзя!");
+                            return;
+                        }
+                        num1 = num1 / num2;
+                        break;
+                    default:
+                        System.out.println("Ошибка ввода оператора!");
+                        return;
+                }
+                System.out.println("Результат: " + num1);
             }
+        } catch (InputMismatchException e) {
+            System.out.println("Ошибка ввода: "+e.getMessage());
         }
-        return mas;
+
     }
 }
+
