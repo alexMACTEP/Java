1a5b741c264605e7e30cb25a51bcaee6b0cf5703
alexMACTEP <a069ae@yandex.ru>
2023-09-17 16:29:26 +0300

    calculator V0.0.2

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,73 @@
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Добро пожаловть в калькулятор");
+        vvodSystemIn();
+    }
 
+    public static void vvodSystemIn() {
+        int i = 0;
+        double num1 = 0;
+        double num2;
+        String operator = "+";
         try {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите  число: ");
-            double num1 = scanner.nextDouble();
-            boolean f = true;
-            while (f) {
-                System.out.print("Введите оператор (В случае выходы введите - 'e'): ");
-                char operator = scanner.next().charAt(0);
-                if (operator == 'e') {
-                    System.out.println("вы завершили работу");
+            while (scanner.hasNextLine()) {
+                String k = scanner.nextLine();
+                if (k.equals("=")) {
                     break;
-                }
-                if (!((operator == ('+')) || (operator == ('-')) || (operator == ('*'))||(operator == ('/')))){
-                    System.out.println("Ошибка ввода");
-                    break;
-                }
-                System.out.print("Введите число: ");
-                double num2 = scanner.nextDouble();
-
-                switch (operator) {
-                    case '+':
-                        num1 = num1 + num2;
-                        break;
-                    case '-':
-                        num1 = num1 - num2;
-                        break;
-                    case '*':
-                        num1 = num1 * num2;
-                        break;
-                    case '/':
-                        if (num2 == 0) {
-                            System.err.println("Делить на ноль нельзя!");
-                            return;
+                } else if (i == 0) {
+                    if (k.matches("(\\-?\\d+\\.?\\d*)")) {
+                        num1 = Double.parseDouble(k);
+                        i++;
+                    } else {
+                        System.out.println("Ошибка ввода, повторите попытку");
+                    }
+                } else {
+                    if (i % 2 != 0) {
+                        if (k.equals("+") || k.equals("-") || k.equals("*") || k.equals("/")) {
+                            operator = k;
+                            i++;
+                        } else {
+                            System.out.println("Ошибка ввода повторите попытку");
                         }
-                        num1 = num1 / num2;
-                        break;
-                    default:
-                        System.out.println("Ошибка ввода оператора!");
-                        return;
+                    } else {
+                        if (k.matches("(\\-?\\d+\\.?\\d*)")) {
+                            num2 = Double.parseDouble(k);
+                            i++;
+                            num1 = calculator(num1, operator, num2);
+                            System.out.println("Ответ=" + num1);
+                        } else {
+                            System.out.println("Ошибка ввода повторите попытку num2");
+                        }
+                    }
                 }
-                System.out.println("Результат: " + num1);
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Ошибка ввода: "+e.getMessage());
+        } catch (
+                InputMismatchException e) {
+            System.out.println("ERROR " + e.getMessage());
         }
+        System.out.println("финальный результат=" + num1);
+    }
+
+    public static double calculator(double num1, String operator, double num2) {
+        switch (operator) {
+            case "+" -> num1 = num1 + num2;
+            case "-" -> num1 = num1 - num2;
+            case "*" -> num1 = num1 * num2;
+            case "/" -> {
+                if (num2 == 0) {
+                    System.err.println("Делить на ноль нельзя!");
+                    break;
+                }
+                num1 = num1 / num2;
+            }
+            default -> System.out.println("Ошибка ввода оператора!");
+        }
+        System.out.println("Результат: " + num1);
+        return num1;
     }
 }
+
 
